ed9f3fe008b53a4f48a641302532f3f2706160e2
Unknown <thijsledeboer@gmail.com>
2018-03-04 10:45:46 +0100

    Updated the git

---
+++ src/cmake-build-debug/CMakeFiles/src.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Study/Data science 2017 - 2018/Database technology/DatabaseTech/src/RPQTree.cpp" "C:/Study/Data science 2017 - 2018/Database technology/DatabaseTech/src/cmake-build-debug/CMakeFiles/src.dir/RPQTree.cpp.obj"
+  "C:/Study/Data science 2017 - 2018/Database technology/DatabaseTech/src/SimpleEstimator.cpp" "C:/Study/Data science 2017 - 2018/Database technology/DatabaseTech/src/cmake-build-debug/CMakeFiles/src.dir/SimpleEstimator.cpp.obj"
+  "C:/Study/Data science 2017 - 2018/Database technology/DatabaseTech/src/SimpleEvaluator.cpp" "C:/Study/Data science 2017 - 2018/Database technology/DatabaseTech/src/cmake-build-debug/CMakeFiles/src.dir/SimpleEvaluator.cpp.obj"
+  "C:/Study/Data science 2017 - 2018/Database technology/DatabaseTech/src/SimpleGraph.cpp" "C:/Study/Data science 2017 - 2018/Database technology/DatabaseTech/src/cmake-build-debug/CMakeFiles/src.dir/SimpleGraph.cpp.obj"
+  "C:/Study/Data science 2017 - 2018/Database technology/DatabaseTech/src/main.cpp" "C:/Study/Data science 2017 - 2018/Database technology/DatabaseTech/src/cmake-build-debug/CMakeFiles/src.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/cmake-build-debug/CMakeFiles/src.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/src.dir/main.cpp.obj"
+  "CMakeFiles/src.dir/RPQTree.cpp.obj"
+  "CMakeFiles/src.dir/SimpleEstimator.cpp.obj"
+  "CMakeFiles/src.dir/SimpleEvaluator.cpp.obj"
+  "CMakeFiles/src.dir/SimpleGraph.cpp.obj"
+  "src.pdb"
+  "src.exe"
+  "src.exe.manifest"
+  "libsrc.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/src.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
